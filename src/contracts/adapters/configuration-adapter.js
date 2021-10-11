b0efbf5d07f47916cee9197791278e5ccc275c48
Joseph C Wang <joequant@gmail.com>
2021-10-11 15:17:18 +0800

    add configuration adapter

---
+++ src/contracts/adapters/configuration-adapter.js
@@ -0,0 +1,78 @@
+const Web3 = require("web3");
+const { ethers } = require("ethers");
+const toBytes32 = ethers.utils.formatBytes32String;
+const { configs } = require("../../../cli-config");
+const { sha3, toBN, proposalIdGenerator } = require("tribute-contracts/utils/ContractUtil");
+const { prepareVoteProposalData } = require("@openlaw/snapshot-js-erc712");
+const { entryDao } = require("tribute-contracts/utils/DeploymentUtil");
+const { getContract } = require("../../utils/contract");
+const { submitSnapshotProposal } = require("../../services/snapshot-service");
+const { parseDaoFlags } = require("../core/dao-registry");
+const {   success,
+  notice,
+  info,
+  logEnvConfigs,
+  error,
+          warn } = require("../../utils/logging");
+
+const submitConfigurationProposal = async (key, value, opts) => {
+  const { contract, provider, wallet } = getContract(
+    "ConfigurationContract",
+    configs.contracts.ConfigurationContract
+  );
+  return await submitSnapshotProposal(
+    `Key: ${key} -> ${value}`,
+    "Creates/Update configuration",
+    configs.contracts.ConfigurationContract,
+    provider,
+    wallet
+  ).then(async (res) => {
+    const data = res.data;
+    const snapshotProposalId = res.uniqueId;
+    const daoProposalId = sha3(snapshotProposalId);
+    const message = {
+      payload: {
+        body: data.payload.body,
+        choices: data.payload.choices,
+        end: data.payload.end,
+        name: data.payload.name,
+        snapshot: data.payload.snapshot.toString(),
+        start: data.payload.start,
+      },
+      sig: res.sig,
+      space: data.space,
+      timestamp: parseInt(data.timestamp),
+    };
+    if (opts.debug) warn(`DAO Message: ${JSON.stringify(message)}\n`);
+
+    const encodedData = prepareVoteProposalData(message, new Web3(""));
+    if (opts.debug) warn(`Encoded DAO message: ${encodedData}\n`);
+
+    await contract.submitProposal(
+        configs.contracts.DaoRegistry,
+        daoProposalId,
+        [sha3(key)],
+        [Number(value)],
+        encodedData ? encodedData : ethers.utils.toUtf8Bytes(""),
+        { from: wallet.address }
+    );
+    return { daoProposalId, snapshotProposalId };
+  });
+    
+};
+
+const processConfigurationProposal = async (daoProposalId) => {
+  const { contract, wallet } = getContract(
+    "ConfigurationContract",
+    configs.contracts.ConfigurationContract
+  );
+
+  await contract.processProposal(configs.contracts.DaoRegistry, daoProposalId, {
+    from: wallet.address,
+  });
+
+  return { daoProposalId };
+};
+
+
+module.exports = { submitConfigurationProposal };
