473c2cc39765af488cff3f4c28d516e0665cd640
Felipe Forbeck <felipe.forbeck@gmail.com>
2021-11-01 16:28:02 -0300

    feat: get ext and config addresses

--- src/interfaces/cli/commands/dao-registry-cmd.js
+++ src/interfaces/cli/commands/dao-registry-cmd.js
@@ -1,7 +1,9 @@
 const { configs } = require("../../../../cli-config");
 const {
   getAdapterAddress,
+  getExtensionAddress,
   getDAOConfig,
+  getDAOConfigAddress,
 } = require("../../../contracts/core/dao-registry");
 const {
   success,
@@ -10,7 +12,6 @@ const {
   logEnvConfigs,
   error,
 } = require("../../../utils/logging");
-const { sha3 } = require("tribute-contracts/utils/ContractUtil");
 
 const daoRegistryCommands = (program) => {
   program
@@ -21,11 +22,28 @@ const daoRegistryCommands = (program) => {
       logEnvConfigs(configs);
       info(`AdapterId:\t\t${adapterId}`);
 
-      return getAdapterAddress(sha3(adapterId))
+      return getAdapterAddress(adapterId)
         .then((data) => {
           success(`Adapter Address: \t${data}\n`);
         })
         .catch((err) => error("Error while getting the adapter address", err));
+    });
+
+  program
+    .command("get-extension-addr <extensionId>")
+    .description("Gets the extension address if configured in the DAO.")
+    .action((extensionId) => {
+      notice(`\n ::: Get extension address...\n`);
+      logEnvConfigs(configs);
+      info(`ExtensionId:\t\t${extensionId}`);
+
+      return getExtensionAddress(extensionId)
+        .then((data) => {
+          success(`Extension Address: \t${data}\n`);
+        })
+        .catch((err) =>
+          error("Error while getting the extension address", err)
+        );
     });
 
   program
@@ -41,6 +59,21 @@ const daoRegistryCommands = (program) => {
           success(`Value: \t\t\t${data}\n`);
         })
         .catch((err) => error("Error while getting the config value", err));
+    });
+
+  program
+    .command("get-config-addr <configName>")
+    .description("Gets the configuration address from the DAO.")
+    .action((configName) => {
+      notice(`\n ::: Get configuration address...\n`);
+      logEnvConfigs(configs);
+      info(`ConfigAddress:\t\t${configName}`);
+
+      return getDAOConfigAddress(configName)
+        .then((data) => {
+          success(`Value: \t\t\t${data}\n`);
+        })
+        .catch((err) => error("Error while getting the config address", err));
     });
 
   return program;
