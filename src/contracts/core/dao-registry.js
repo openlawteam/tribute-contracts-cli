84fb8494789271957b35a30ef77106ff168775a3
Felipe Forbeck <felipe.forbeck@gmail.com>
2022-01-13 16:26:53 -0300

    dao-registry v2.3.3

--- src/contracts/core/dao-registry.js
+++ src/contracts/core/dao-registry.js
@@ -2,32 +2,34 @@ import { sha3 } from "tribute-contracts/utils/contract-util.js";
 import { getContract } from "../../utils/contract.js";
 import { configs } from "../../../cli-config.js";
 
+const CONTRACT_NAME = "DaoRegistry";
+
 export const getDAOConfig = async (configKey) => {
-  const { contract } = getContract("DaoRegistry", configs.dao);
+  const { contract } = getContract(CONTRACT_NAME, configs.dao);
   return await contract.getConfiguration(sha3(configKey));
 };
 
 export const getDAOConfigAddress = async (configKey) => {
-  const { contract } = getContract("DaoRegistry", configs.dao);
+  const { contract } = getContract(CONTRACT_NAME, configs.dao);
   return await contract.getAddressConfiguration(sha3(configKey));
 };
 
 export const getAddressIfDelegated = async (memberAddress) => {
-  const { contract } = getContract("DaoRegistry", configs.dao);
+  const { contract } = getContract(CONTRACT_NAME, configs.dao);
   return await contract.getAddressIfDelegated(memberAddress);
 };
 
 export const getMemberAddress = async (memberIndex) => {
-  const { contract } = getContract("DaoRegistry", configs.dao);
+  const { contract } = getContract(CONTRACT_NAME, configs.dao);
   return await contract.getMemberAddress(memberIndex);
 };
 
 export const getAdapterAddress = async (adapterId) => {
-  const { contract } = getContract("DaoRegistry", configs.dao);
+  const { contract } = getContract(CONTRACT_NAME, configs.dao);
   return await contract.getAdapterAddress(sha3(adapterId));
 };
 
 export const getExtensionAddress = async (extensionId) => {
-  const { contract } = getContract("DaoRegistry", configs.dao);
+  const { contract } = getContract(CONTRACT_NAME, configs.dao);
   return await contract.getExtensionAddress(sha3(extensionId));
 };
