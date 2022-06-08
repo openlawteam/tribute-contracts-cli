542f12f6ac765237691fb7ec6fe78b3995694b95
Nicholas McGuire <nickmcguire.zero@gmail.com>
2022-06-08 15:13:52 -0500

    Add command for Manager adapter

---
+++ src/contracts/adapters/manager-adapter.js
@@ -0,0 +1,161 @@
+import { sha3 } from "tribute-contracts/utils/contract-util.js";
+import {
+  adaptersIdsMap,
+} from "tribute-contracts/utils/dao-ids-util.js";
+import {
+  entryDao,
+  getEnabledExtensionFlags,
+} from "tribute-contracts/utils/access-control-util.js";
+import { configs } from "../../../cli-config.js";
+import { getAdapter } from "../../utils/contract.js";
+import { parseConfigs } from "./configuration-adapter.js";
+import { GcpKmsSigner, TypedDataVersion } from "ethers-gcp-kms-signer";
+import { availableExtensions } from './managing-adapter';
+import { getAdapterAddress } from '../core/dao-registry.js';
+
+const CONTRACT_NAME = "Manager";
+
+export const submitAndProcessProposal = async ({
+  adapterOrExtensionId,
+  adapterOrExtensionAddress,
+  updateType,
+  aclFlags,
+  numericConfigKeys,
+  numericConfigValues,
+  extensions,
+  configurations,
+}) => {
+  const configKeys = numericConfigKeys
+    ? numericConfigKeys.map((k) => sha3(k))
+    : [];
+  const configValues = numericConfigValues ? numericConfigValues : [];
+  const configAclFlags = aclFlags ? new Array(...aclFlags) : new Array();
+  const { extensionAddresses, extensionAclFlags } = Object.values(extensions).reduce((acc, e) => {
+    acc.extensionAddresses.push(e.data.address);
+    const allAclsForExtension = availableExtensions[e.extensionId];
+    acc.extensionAclFlags.push(
+      getEnabledExtensionFlags(
+        allAclsForExtension,
+        e.extensionId,
+        e.data.address,
+        { extensions: { [e.extensionId]: e.data.flags } }
+      ).flags
+    );
+    return acc;
+  }, { extensionAddresses: [], extensionAclFlags: [] });
+  const daoConfigurations = configurations ? parseConfigs(configurations) : [];
+
+  // TODO: uncomment after new tribute-contracts release
+  // const { contract: managerAdapter, provider } = await getAdapter(adaptersIdsMap.MANAGER, CONTRACT_NAME);
+  const { contract: managerAdapter, provider } = await getAdapter('manager', CONTRACT_NAME);
+  const { chainId } = await provider.getNetwork();
+  const nonce = (await managerAdapter.nonces(configs.dao)).toNumber() + 1;
+
+  const flags = entryDao(adapterOrExtensionId, adapterOrExtensionAddress, {
+    dao: configAclFlags,
+  }).flags;
+  const updateTypeResolved = await parseUpdateType(updateType);
+  
+  const proposal = {
+    adapterOrExtensionId: sha3(adapterOrExtensionId),
+    adapterOrExtensionAddr: adapterOrExtensionAddress,
+    flags,
+    updateType: updateTypeResolved,
+    keys: configKeys,
+    values: configValues,
+    extensionAddresses,
+    extensionAclFlags,
+  };
+  const proposalId = sha3(configs.dao + nonce.toString());
+
+  const signature = await getSignature(
+    {
+      daoAddress: configs.dao,
+      proposal,
+      configs: daoConfigurations,
+      nonce,
+      proposalId,
+    },
+    managerAdapter.address,
+    chainId,
+  );
+
+  await managerAdapter.processSignedProposal(
+    configs.dao,
+    proposalId,
+    proposal,
+    daoConfigurations,
+    nonce,
+    signature,
+    { gasLimit: 2100000 }
+  );
+
+  const updatedAddress = await getAdapterAddress(adapterOrExtensionId);
+  return { proposalId, updatedAddress };
+};
+
+const getSignature = async (proposalCouponData, managerAdapterAddress, chainId) => {
+  const domain = {
+    name: "Snapshot Message",
+    version: "4",
+    chainId,
+    verifyingContract: configs.dao,
+    actionId: managerAdapterAddress,
+  };
+  const types = {
+    Message: [
+      { name: "daoAddress", type: "address" },
+      { name: "proposal", type: "ProposalDetails" },
+      { name: "configs", type: "Configuration[]" },
+      { name: "nonce", type: "uint256" },
+      { name: "proposalId", type: "bytes32" },
+    ],
+    ProposalDetails: [
+      { name: "adapterOrExtensionId", type: "bytes32" },
+      { name: "adapterOrExtensionAddr", type: "address" },
+      { name: "updateType", type: "uint8" },
+      { name: "flags", type: "uint128" },
+      { name: "keys", type: "bytes32[]" },
+      { name: "values", type: "uint256[]" },
+      { name: "extensionAddresses", type: "address[]" },
+      { name: "extensionAclFlags", type: "uint128[]" },
+    ],
+    Configuration: [
+      { name: "key", type: "bytes32" },
+      { name: "numericValue", type: "uint256" },
+      { name: "addressValue", type: "address" },
+      { name: "configType", type: "uint8" },
+    ],
+    EIP712Domain: [
+      { name: "name", type: "string" },
+      { name: "version", type: "string" },
+      { name: "chainId", type: "uint256" },
+      { name: "verifyingContract", type: "address" },
+      { name: "actionId", type: "address" },
+    ],
+  };
+
+  const signer = new GcpKmsSigner(configs.kmsCredentials);
+  const signature = await signer.signTypedData({
+    data: {
+      types,
+      primaryType: "Message",
+      domain,
+      message: proposalCouponData,
+    },
+    version: TypedDataVersion.V4,
+  });
+
+  return signature;
+};
+
+const parseUpdateType = async (value) => {
+  switch (value) {
+    case "Adapter":
+      return 1;
+    case "Extension":
+      return 2;
+    default:
+      throw Error(`Unknown update type: ${value}`);
+  }
+};
---
+++ src/interfaces/cli/commands/manager-cmd.ts
@@ -0,0 +1,180 @@
+import inquirer from "inquirer";
+import { ethers } from "ethers";
+import { daoAccessFlags } from "tribute-contracts/utils/access-control-util.js";
+import { extensionsIdsMap } from "tribute-contracts/utils/dao-ids-util.js";
+import {
+  availableExtensions,
+} from "../../../contracts/adapters/managing-adapter.js";
+import {
+  success,
+  notice,
+  info,
+  logEnvConfigs,
+  error,
+} from "../../../utils/logging.js";
+import { configs } from "../../../../cli-config.js";
+import { collectConfigs } from "./configuration-cmd.js";
+import { getExtensionAddress } from "../../../contracts/core/dao-registry.js";
+import { submitAndProcessProposal } from '../../../contracts/adapters/manager-adapter.js';
+
+export const managerCommands = (program) => {
+  program
+    .command(
+      "manager-submitAndProcessProposal <adapterOrExtensionId> <adapterOrExtensionAddr>"
+    )
+    .description("Submit and process a new managing proposal.")
+    .action(async (adapterOrExtensionId, adapterOrExtensionAddress) => {
+      await inquirer
+        .prompt([
+          {
+            type: "list",
+            name: "updateType",
+            message: `Which type of contract do you want to update?`,
+            choices: ["Adapter", "Extension"],
+          },
+          {
+            type: "checkbox",
+            message: (a) =>
+              `Which DAO permissions does the ${a.updateType} need?`,
+            name: "aclFlags",
+            choices: daoAccessFlags.map((f) => Object.assign({ name: f })),
+          },
+        ])
+        .then(async (answers) => {
+          const { extensions } = await promptExtensionAccessFlags(
+            adapterOrExtensionId
+          );
+
+          const { configurations } = await promptDaoConfigurations(
+            adapterOrExtensionId
+          );
+
+          notice(`\n ::: Submitting and Processing Managing proposal...\n`);
+          logEnvConfigs(configs);
+          info(
+            `Adapter:\t\t${adapterOrExtensionId} @ ${adapterOrExtensionAddress}`
+          );
+          info(`DAO AccessFlags:\t${JSON.stringify(answers.aclFlags)}`);
+          info(
+            `DAO Configurations:\t${
+              configurations ? JSON.stringify(configurations) : "n/a"
+            }\n`
+          );
+          info(
+            `Extensions:\t\t${extensions ? JSON.stringify(extensions) : "n/a"}`
+          );
+
+          return submitAndProcessProposal({
+            adapterOrExtensionId,
+            adapterOrExtensionAddress,
+            updateType: answers.updateType,
+            aclFlags: answers.aclFlags,
+            numericConfigKeys: [], // empty because the configs are now collected using the configurations obj
+            numericConfigValues: [], // empty because the configs are now collected using the configurations obj
+            extensions,
+            configurations,
+          });
+        })
+        .then((data) => {
+          success(`adapterOrExtension ${adapterOrExtensionId} updated to address ${adapterOrExtensionAddress}\n`);
+          success(`::: Managing proposal submitted and processed!\n`, true);
+        })
+        .catch((err) => error("Error while submitting managing proposal", err));
+    });
+
+  return program;
+};
+
+const collectExtensionAccessFlags = async (inputs = []) => {
+  const prompts = [
+    {
+      type: "list",
+      name: "extensionId",
+      message: `Which extension do you want to grant access to?`,
+      choices: Object.values(extensionsIdsMap),
+    },
+    {
+      type: "checkbox",
+      name: "data",
+      choices: (answers) => availableExtensions[answers.extensionId],
+      message: (answers) =>
+        `Select the ACL Flags to grant the access to the ${answers.extensionId} extension:`,
+      filter: async (selectedAclFlags, answers) => {
+        const extensionAddr = await getExtensionAddress(answers.extensionId);
+        return {
+          address: extensionAddr,
+          flags: selectedAclFlags,
+        };
+      },
+    },
+    {
+      type: "confirm",
+      name: "repeat",
+      message: "Grant access to another extension? ",
+      default: false,
+    },
+  ];
+
+  const { repeat, ...answers } = await inquirer.prompt(prompts);
+  const newInputs = [...inputs, answers];
+  return repeat ? collectExtensionAccessFlags(newInputs) : newInputs;
+};
+
+const collectNumericConfigs = async (inputs = []) => {
+  const prompts = [
+    {
+      type: "input",
+      name: "configKey",
+      message: `Type the DAO numeric configuration name:`,
+    },
+    {
+      type: "input",
+      name: "configValue",
+      message: () => `Type the DAO numeric configuration value:`,
+      validate: (input) => {
+        if (ethers.BigNumber.from(input)) return true;
+        return "Not a number";
+      },
+    },
+    {
+      type: "confirm",
+      name: "repeat",
+      message: "Enter another numeric config? ",
+      default: false,
+    },
+  ];
+
+  const { repeat, ...answers } = await inquirer.prompt(prompts);
+  const newInputs = [...inputs, answers];
+  return repeat ? collectNumericConfigs(newInputs) : newInputs;
+};
+
+const promptExtensionAccessFlags = async (adapterOrExtensionId) => {
+  const { requiresExtAccess } = await inquirer.prompt([
+    {
+      type: "confirm",
+      name: "requiresExtAccess",
+      message: `Does the ${adapterOrExtensionId} contract require access to any extension?`,
+      default: false,
+    },
+  ]);
+  let extensions = {};
+  if (requiresExtAccess) {
+    extensions = await collectExtensionAccessFlags();
+  }
+  return { extensions };
+};
+
+const promptDaoConfigurations = async (adapterOrExtensionId) => {
+  const { requiresConfigs } = await inquirer.prompt([
+    {
+      type: "confirm",
+      name: "requiresConfigs",
+      message: `Does the ${adapterOrExtensionId} contract require any DAO configuration parameter?`,
+      default: false,
+    },
+  ]);
+
+  const configurations = requiresConfigs ? await collectConfigs() : [];
+  return { configurations };
+};
